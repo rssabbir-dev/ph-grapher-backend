b45bbe2725292cc93accd8f5befe34411222604c
rssabbir <sabbirhossainrahad@gmail.com>
2022-11-08 13:52:05 +0600

    added limit on /services

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
 //Importing Package and Library
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 //Use Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //Default or Home path of server
 app.get('/', (req, res) => {
-    res.send('Ph_Grapher server is running')
-})
+	res.send('Ph_Grapher server is running');
+});
 
 //MongoDB Client
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z9hjm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
 //Client Start Function
 const run = async () => {
-    const database = client.db('phGrapherDB');
-    const serviceCollection = database.collection('services')
+	const database = client.db('phGrapherDB');
+	const serviceCollection = database.collection('services');
 
-    //Get All Services
-    app.get('/services', async(req, res) => {
-        const query = {}
-        const services = await serviceCollection.find(query).toArray();
-        res.send(services)
-    })
+	//Get All Services
+	app.get('/services', async (req, res) => {
+		const limit = parseInt(req.query.limit);
+		const query = {};
+		const count = await serviceCollection.estimatedDocumentCount();
+		if (limit) {
+			const services = await serviceCollection
+				.find(query)
+				.limit(limit)
+				.toArray();
+			res.send(services);
+		} else {
+			const services = await serviceCollection.find(query).toArray();
+			res.send(services);
+		}
+	});
 
-    //Get a Single Service
-    app.get('/service/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const service = await serviceCollection.findOne(query);
-        res.send(service)
-    })
-}
+	//Get a Single Service
+	app.get('/service/:id', async (req, res) => {
+		const id = req.params.id;
+		const query = { _id: ObjectId(id) };
+		const service = await serviceCollection.findOne(query);
+		res.send(service);
+	});
+};
 //Client Start
-run().catch(err => console.log(err))
+run().catch((err) => console.log(err));
 
-//Listen server on PORT 
+//Listen server on PORT
 app.listen(port, () => {
-    console.log(`Ph_Grapher server is running on ${port}`);
-})
+	console.log(`Ph_Grapher server is running on ${port}`);
+});
